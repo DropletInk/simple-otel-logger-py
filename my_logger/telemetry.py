cb7ecda07a9dfa55e31814e01a6ca9921972875b
Devang Kamdar <devangkamdar65@gmail.com>
2026-06-03 18:36:44 +0530

    added custom span Feature

--- my_logger/telemetry.py
+++ my_logger/telemetry.py
@@ -4,3 +4,9 @@ from opentelemetry.sdk.trace import TracerProvider
 
 def setup_tracing():
     trace.set_tracer_provider(TracerProvider())
+
+
+def create_span(name):
+    tracer = trace.get_tracer("my_logger")
+
+    return tracer.start_as_current_span(name)
